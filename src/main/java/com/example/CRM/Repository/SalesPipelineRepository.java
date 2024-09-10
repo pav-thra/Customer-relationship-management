e34218b65b9a87ef445fe4f16f8d265ef3959f51
Pavithra <v.pavithrav1011@gmail.com>
2024-09-10 23:15:45 +0530

    Created Controller classes and updated repository interfaces

--- src/main/java/com/example/CRM/Repository/SalesPipelineRepository.java
+++ src/main/java/com/example/CRM/Repository/SalesPipelineRepository.java
@@ -3,5 +3,8 @@ package com.example.CRM.Repository;
 import com.example.CRM.Entities.Sales_Pipeline;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SalesPipelineRepository extends JpaRepository<Sales_Pipeline, Integer> {
+    List<Sales_Pipeline> findByStage(String stage);
 }
--- src/main/java/com/example/CRM/Repository/SupportTicketRepository.java
+++ src/main/java/com/example/CRM/Repository/SupportTicketRepository.java
@@ -3,5 +3,8 @@ package com.example.CRM.Repository;
 import com.example.CRM.Entities.Support_Tickets;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SupportTicketRepository extends JpaRepository<Support_Tickets, Integer> {
+    List<Support_Tickets> findByStatus(String status);
 }
